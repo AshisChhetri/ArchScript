d7c56eac30a0eb9a163cc9ba74e97b0c5db659f6
Ashis Thapa Chhetri <ashis2015back@gmail.com>
2023-08-31 05:35:55 +0000

    some change

--- 2-configuration.sh
+++ 2-configuration.sh
@@ -84,7 +84,7 @@ systemctl enable NetworkManager
 # Grub installation
 # ------------------------------------------------------
 pacman --noconfirm -S grub efibootmgr os-prober 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi
 grub-mkconfig -o /boot/grub/grub.cfg
 
 
